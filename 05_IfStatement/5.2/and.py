4287cda154a3ebe94367020b034da65bf046c5f1
Junhui Li <junhui.lee@icloud.com>
2023-11-06 10:37:47 +0800

    FInished Chapter 05

---
+++ 05_IfStatement/5.2/and.py
@@ -0,0 +1,15 @@
+num1 = 50
+num2 = 100
+
+if num1 + num2 > 110:
+    print(num1 + num2 > 100)
+
+if (num1 > 20 or num2 > 50):
+    print("Or")
+
+requested_toppings = ['mushroom', 'onions', 'pineapple']
+if 'onions' in requested_toppings:
+    print('true')
+
+if 'apple' not in requested_toppings:
+    print('true')
---
+++ 05_IfStatement/5.3/5-3.py
@@ -0,0 +1,8 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You got 5 points")
+elif alien_color == 'yello':
+    print("You got 10 points")
+else:
+    print("You got nothing")
